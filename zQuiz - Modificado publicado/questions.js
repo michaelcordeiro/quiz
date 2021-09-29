2eeebf2251911571ccc6f5d0b526e0531701771a
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2021-09-29 12:22:19 -0300

    quiz

---
+++ zQuiz - Modificado publicado/questions.js
@@ -0,0 +1,106 @@
+let questions = [
+    {
+        question: 'De acordo com as disposições do Código Penal (CP) brasileiro, é CORRETO afirmar que:',
+        options: [
+            'Para efeito de configuração do crime de furto (art. 155 do CP), a energia elétrica é equiparada à coisa móvel.',
+            'O crime de roubo (art. 157 do CP) consiste na conduta de subtrair, para si ou para outrem, coisa alheia móvel, sem o emprego de violência ou de grave ameaça à pessoa.',
+            'O emprego de arma de fogo para o exercício da violência ou da grave ameaça, no crime de roubo (art. 157 do CP), é uma circunstância irrelevante para fins de aplicação da pena.',
+            'Aquele que se apropria de coisa alheia móvel, de que tem a posse ou a detenção, pratica o crime de furto (art. 155 do CP).'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Assinale a alternativa CORRETA que indica os crimes contra a honra:',
+        options: [
+            'Estupro, atentado violento ao pudor, corrupção de menores.',
+            'Ameaça, constrangimento ilegal, ato obsceno.',
+            'Calúnia, injúria e difamação.',
+            'Abandono de posto, embriaguez em serviço, dormir em serviço.'
+        ],
+        answer: 2
+    },
+    {
+        question: 'O Código Penal estabelece o rol dos crimes contra a pessoa. Assinale a alternativa que NÃO corresponde a um crime contra a pessoa:',
+        options: [
+            'Homicídio simples.',
+            'Genocídio.',
+            'Lesão leve.',
+            'Latrocínio.'
+        ],
+        answer: 3
+    },
+    {
+        question: 'NÃO é falta grave, prevista na Lei de Execução Penal, cometida pelo condenado à pena privativa de liberdade:',
+        options: [
+            'Fugir.',
+            'Provocar acidente de trabalho.',
+            'Descumprir, no regime aberto, as condições impostas.',
+            'Retardar, injustifcadamente, o cumprimento da obrigação imposta.'
+        ],
+        answer: 3
+    },
+    {
+        question: 'São órgãos da execução penal, EXCETO:',
+        options: [
+            'A Defensoria Pública.',
+            'Os Departamentos Penitenciários.',
+            'A Ordem dos Advogados do Brasil.',
+            'O Ministério Público.'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Nos termos da Lei de Execução Penal, são exemplos de penas restritivas de direitos:',
+        options: [
+            'pagamento de cesta básica e multa.',
+            'prestação de serviços à comunidade e limitação de fim de semana.',
+            'interdição temporária de direitos e trabalhos forçados.',
+            'prisão simples e banimento.',
+            'reclusão e detenção.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Às presidiárias serão asseguradas condições para que possam permanecer com seus filhos durante:',
+        options: [
+            'seis meses.',
+            'cento e oitenta dias.',
+            'quatro meses.',
+            'cento e vinte dias.',
+            'o período de amamentação.'
+        ],
+        answer: 4
+    },
+    {
+        question: 'A supervisão dos patronatos, bem como a assistência aos egressos, incumbe ao(à):',
+        options: [
+            'juízo da execução penal.',
+            'direção do estabelecimento penal.',
+            'Conselho Penitenciário.',
+            'Departamento Penitenciário Nacional.',
+            'Ministério Público.'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Em conformidade com a Lei de Execução Penal, a assistência ao egresso consiste na orientação e apoio para:',
+        options: [
+            'promover a sua reeducação.',
+            'inseri-lo no sistema educacional.',
+            'propiciar-lhe atividades religiosas.',
+            'reintegrá-lo à vida em liberdade.'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Nos termos da Lei de Execução Penal (Lei Federal nº 7.210/84), o trabalho interno não será obrigatório para o preso:',
+        options: [
+            'em regime fechado',
+            'em regime de internato',
+            'que exercer atividades complementares',
+            'provisório',
+            'que possuir a respectiva capacitação'
+        ],
+        answer: 3
+    },
+];
